d7e3660cca5bfca8af8e3c97f7cefbe0ab7a937f
Supercito-132 <83719009+Supercito-132@users.noreply.github.com>
2023-04-11 11:39:44 -0600

    Api 2.0
    Cambios de Hugod

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const multer = require('multer');
+import express from 'express';
+import cors from 'cors';
+import multer from 'multer';
+import fetch from 'node-fetch'
 
 const port = process.env.PORT || 3013;
 
