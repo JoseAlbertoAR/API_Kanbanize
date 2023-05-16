f3c69ed642f6d93844a6d82fd23d9beef3b6a47d
Alejandro Suarez <suga.gonza.18@gmail.com>
2023-05-16 14:56:37 -0600

    Endpoint de users y card/create funcionales

--- index.js
+++ index.js
@@ -159,10 +159,8 @@ app.post('/columns', async (req, res) => {
 
 app.post('/cards', async (req, res) => {
     const apikey = req.body.apikey;
-    const w_ID = 47; //req.params.w_ID; //workflow_id
-    const b_ID = 21; //req.params.b_ID; //board_id
-    const c_ID = 329; //req.params.c_ID; //column_id
-
+    // const b_ID = 21; //req.params.b_ID; //board_id
+    const b_ID = req.body.b_ID; //board_id
 
     try {
         // const response = await fetch(`https://university6y.kanbanize.com/api/v2/cards?board_ids=${b_ID}&workflow_ids=${w_ID}&column_ids=${c_ID}&fields=card_id,title,priority,deadline,board_id,workflow_id,column_id&expand=co_owner_ids`, {
@@ -188,24 +186,67 @@ app.post('/cards', async (req, res) => {
     }
 })
 
-app.post('/cards/create', async (req, res) => {
+app.post('/users', async (req, res) => {
     const apikey = req.body.apikey;
-    const values = {column_id: req.body.c_ID, lane_id: req.body.w_ID, title: req.body.title, deadline: req.body.deadline, owner_user_id: req.body.owner_user_id, priority: req.body.priority}
+
+    try {
+        const response = await fetch(`https://university6y.kanbanize.com/api/v2/users?fields=user_id,username,realname&expand=board_roles`, {
+            method: "get",
+            headers: {
+                "apikey": apikey
+            },
+        })
+        if (response.ok) {
+            const data = await response.json();
+            res.json(data);
+            // console.log("Users: ", data);
+        } else {
+            res.json({ "error": response.status });
+        }
+    } catch (err) {
+        console.error(err);
+        res.json({ "error": err });
+    }
+})
+
+app.post('/cards/create', async (req, res) => {
+    //esta ruta crea una tarjeta en el tablero
+    const apikey = req.headers.apikey;
+    //const values = {column_id: req.body.c_ID, lane_id: req.body.w_ID, title: req.body.title, deadline: req.body.deadline, owner_user_id: req.body.owner_user_id, priority: req.body.priority}
+    //const values = {column_id: req.body.c_ID, lane_id: req.body.w_ID, title: req.body.title}
+
+    const column_id = req.body.column_id;
+    const lane_id = req.body.lane_id;
+    const title = req.body.title;
+    const deadline = req.body.deadline;
+    const owner_user_id = req.body.owner_user_id;
+    const priority = req.body.priority;
+
+    const formData = JSON.stringify({
+        "column_id": column_id,
+        "lane_id": lane_id,
+        "title": title,
+        "deadline": deadline,
+        "owner_user_id": owner_user_id,
+        "priority": priority
+    })
+
     try {
         const response = await fetch(`https://university6y.kanbanize.com/api/v2/cards`,
         {
             method: "POST",
             headers: {
-                "Content-Type": "application/json", charset: "utf-8",
+                "Content-Type": "application/json; charsetx=utf8",
                 "apikey": apikey
             },
-            body: JSON.stringify(values)
+            // body: JSON.stringify(values)
+            body: formData,
 
-        })
+        });
         if (response.ok) {
             const data = await response.json();
             res.json(data);
-            //console.log("Card: ", data);
+            //console.log("Boards: ", data);
         }
         else {
             res.json({ "error": response.status });
