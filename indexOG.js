76113bfb1186969be4cc2beb57bd2b2625efd4ef
Supercito-132 <83719009+Supercito-132@users.noreply.github.com>
2023-05-31 20:56:01 -0600

    Versión con API solo Local sin dependencias de AWS

--- indexOG.js
+++ indexOG.js
@@ -1,6 +1,163 @@
-const fetch = require('node-fetch');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+require('dotenv/config');
 
-module.exports.cards = async (req, res) => {
+const port = process.env.PORT || 3013;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('/login', async (req, res) => {
+    console.log('body: ', req.body);
+    const values = {
+        email: req.body.email,
+        pass: req.body.pass
+    };
+    const response = await fetch(`https://university6y.kanbanize.com/index.php/api/kanbanize/login//format/json`,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(values)
+        });
+    const data = await response.json();
+    res.json(data);
+})
+
+app.get('/', async (req, res) => {
+    /*const values = {
+        email: req.body.email, 
+        pass: req.body.pass
+    };*/
+    const values = {
+        email: "matricula@tec.mx",
+        pass: "contraseña"
+    };
+    const response = await fetch(`https://university6y.kanbanize.com/index.php/api/kanbanize/login//format/json`,
+        {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(values)
+
+        })
+    const data = await response.json();
+    console.log(values);
+    res.json(data);
+})
+
+app.post('/workspaces', async (req, res) => {
+    const apikey = req.body.apikey;
+    console.log(req.body);
+
+    try {
+        const response = await fetch(`https://university6y.kanbanize.com/api/v2/workspaces?if_assigned_to_boards=1&is_archived=0`, {
+            method: "get",
+            headers: {
+                "apikey": apikey,
+                // "workspace_id": workspace_id
+            },
+        })
+        if (response.ok) {
+            const data = await response.json();
+            //const workSpaces = data.data;
+            res.json(data);
+            console.log("Workspaces: ", data);
+        }
+        else {
+            res.json({ "error": response.status });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ "error": error });
+    }
+})
+
+app.post('/boards', async (req, res) => {
+    const apikey = req.body.apikey;
+
+    try {
+        const response = await fetch(`https://university6y.kanbanize.com/api/v2/boards?is_archived=0&if_assigned=1&fields=board_id,workspace_id,name&expand=workflows`, {
+            method: "get",
+            headers: {
+                "apikey": apikey
+            },
+
+        })
+        if (response.ok) {
+            const data = await response.json();
+            res.json(data);
+            console.log("Boards: ", data);
+        }
+        else {
+            res.json({ "error": response.status });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ "error": error });
+    }
+})
+
+app.post('/workflows', async (req, res) => {
+    const apikey = req.body.apikey;
+    // const b_ID = 21;
+    const b_ID = req.body.b_ID; //board_id
+
+    try {
+        const response = await fetch(`https://university6y.kanbanize.com/api/v2/boards/${b_ID}/workflows`, {
+            method: "get",
+            headers: {
+                "apikey": apikey
+            },
+        })
+        if (response.ok) {
+            const data = await response.json();
+            res.json(data);
+            console.log("Workflows: ", data);
+        } else {
+            res.json({ "error": response.status });
+        }
+    } catch (error) {
+        console.error(error);
+        res.json({ "error": error });
+    }
+    console.log("Board ID: ", b_ID);
+})
+
+
+app.post('/columns', async (req, res) => {
+    const apikey = req.body.apikey;
+    // const b_ID = 21; //req.params.b_ID; //board_id
+    const b_ID = req.body.b_ID; //board_id
+
+
+    try {
+        const response = await fetch(`https://university6y.kanbanize.com/api/v2/boards/${b_ID}/columns?fields=column_id,workflow_id,parent_column_id,name`, {
+            method: "get",
+            headers: {
+                "apikey": apikey
+            },
+
+        })
+        if (response.ok) {
+            const data = await response.json();
+            res.json(data);
+            console.log("Boards: ", data);
+        }
+        else {
+            res.json({ "error": response.status });
+        }
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ "error": error });
+    }
+})
+
+
+app.post('/cards', async (req, res) => {
     const apikey = req.body.apikey;
     // const b_ID = 21; //req.params.b_ID; //board_id
     const b_ID = req.body.b_ID; //board_id
@@ -27,9 +184,9 @@ module.exports.cards = async (req, res) => {
         console.error(error);
         res.json({ "error": error });
     }
-}
+})
 
-module.exports.users = async (req, res) => {
+app.post('/users', async (req, res) => {
     const apikey = req.body.apikey;
 
     try {
@@ -50,9 +207,9 @@ module.exports.users = async (req, res) => {
         console.error(err);
         res.json({ "error": err });
     }
-}
+})
 
-module.exports.cardsCreate = async (req, res) => {
+app.post('/cards/create', async (req, res) => {
     //esta ruta crea una tarjeta en el tablero
     const apikey = req.headers.apikey;
     //const values = {column_id: req.body.c_ID, lane_id: req.body.w_ID, title: req.body.title, deadline: req.body.deadline, owner_user_id: req.body.owner_user_id, priority: req.body.priority}
@@ -99,7 +256,7 @@ module.exports.cardsCreate = async (req, res) => {
         console.error(error);
         res.json({ "error": error });
     }
-}
+})
 
 // app.post('/cards/move', async (req, res) => {
 //     const apikey = req.body.apikey;
@@ -145,8 +302,7 @@ module.exports.cardsCreate = async (req, res) => {
 //         res.json({ "error": error });
 //     }
 // })
-
-module.exports.cardsComments = async (req, res) => {
+app.post('/cards/comments', async (req, res) => {
     const apikey = req.body.apikey;
     // const b_ID = 21; //req.params.b_ID; //board_id
     const card_id = req.body.c_ID; //board_id
@@ -171,9 +327,9 @@ module.exports.cardsComments = async (req, res) => {
         console.error(error);
         res.json({ "error": error });
     }
-}
+})
 
-module.exports.cardsCommentsCreate = async (req, res) => {
+app.post('/cards/comments/create', async (req, res) => {
     //esta ruta crea un comentario en una tarjeta
     const apikey = req.headers.apikey;
     const card_id = req.body.c_ID;
@@ -206,8 +362,8 @@ module.exports.cardsCommentsCreate = async (req, res) => {
         console.error(error);
         res.json({ "error": error });
     }
-}
+})
 
-// app.listen(port, () => {
-//     console.log(`Servidor iniciado en el puerto ${port}`)
-// });
+app.listen(port, () => {
+    console.log(`Servidor iniciado en el puerto ${port}`)
+});
